311d1effc4f68a61e81d86bbe1626510a4c09372
Ayarn Modi <iamayarn2001@gmail.com>
2025-04-27 17:04:40 +0530

    config issue fixed

--- src/configuration.js
+++ src/configuration.js
@@ -3,9 +3,9 @@ let BACKEND_URL = "";
 if (process.env.NODE_ENV === "development") {
   BACKEND_URL = "http://localhost:5000/api";
 } else if (process.env.NODE_ENV === "production") {
-  BACKEND_URL = "https://namo-chatbot.vercel.app/api";
+  BACKEND_URL = "https://namo-chat.vercel.app/api";
 } else {
-  BACKEND_URL = "https://namo-chatbot.vercel.app/api";
+  BACKEND_URL = "https://namo-chat.vercel.app/api";
 }
 
 export default BACKEND_URL;
